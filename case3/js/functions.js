54bec6942596533b66e095e7000b45ec65795d98
zen <12.zeinirfansyah@gmail.com>
2023-02-23 20:27:42 +0700

    add: product, about, and order-form

--- case3/js/functions.js
+++ case3/js/functions.js
@@ -54,30 +54,79 @@ const products = [
     stock: 150,
     description: 'Salak adalah buah yang memiliki rasa manis dan segar. Salak juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
     foto: '../assets/kurma.jpg'
-  },
+  }
 ];
 
+// const productContainer = document.querySelector(".products");
+// let displayProduct = products.map(function (product) {
+//   return `<div class="buah card mb-3 mx-auto">
+//   <img src="${product.foto}" class="card-img-top"/>
+//   <div class="card-body">
+//     <div class="tipeBuah">${product.fruitType}</div>
+//     <br>
+//     <h5 class="card-title">${product.fruitName}</h5>
+//     <p class="card-text">
+//       Stok: ${product.stock} <br>
+//       <div class="fruit-description ">${product.description}</div>
+//     </p>
+//   </div>
+// </div>`;
+// });
 
-const itemsPerPage = 6;
-const numberOfPages = Math.ceil(products.length / itemsPerPage);
-const pagination = document.querySelector(".pagination");
+// displayProduct = displayProduct.join("");
+// productContainer.innerHTML = displayProduct;
+
+const productsPerPage = 4;
 let currentPage = 1;
 
-const productContainer = document.querySelector(".products");
-let displayProduct = products.map(function (product) {
-  return `<div class="buah card mb-3 mx-auto">
-  <img src="${product.foto}" class="card-img-top"/>
-  <div class="card-body">
-    <h5 class="card-title">${product.fruitName}</h5>
-    <p class="card-text">
-      Stok: ${product.stock} <br>
-      <div class="fruit-description d-none d-lg-block">${product.description}</div>
-      <br>
+function displayProducts(products) {
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const displayedProducts = products.slice(startIndex, endIndex);
+  const productContainer = document.querySelector(".products");
+  let displayProduct = displayedProducts.map(function (product) {
+    return `<div class="buah card mb-3 mx-auto">
+    <img src="${product.foto}" class="card-img-top"/>
+    <div class="card-body">
       <div class="tipeBuah">${product.fruitType}</div>
-    </p>
-  </div>
-</div>`;
+      <br>
+      <h5 class="card-title">${product.fruitName}</h5>
+      <p class="card-text">
+        Stok: ${product.stock} <br>
+        <div class="fruit-description ">${product.description}</div>
+      </p>
+    </div>
+  </div>`;
+  });
+  displayProduct = displayProduct.join("");
+  productContainer.innerHTML = displayProduct;
+}
+
+function renderPagination(products) {
+  const totalPages = Math.ceil(products.length / productsPerPage);
+  const paginationContainer = document.querySelector(".pagination");
+  let paginationButtons = "";
+  for (let i = 1; i <= totalPages; i++) {
+    paginationButtons += `<button class="pagination-btn mx-1 ${
+      i === currentPage ? "active" : ""
+    }" data-page="${i}">${i}</button>`;
+  }
+  paginationContainer.innerHTML = paginationButtons;
+}
+
+// inisialisasi tampilan 
+displayProducts(products);
+renderPagination(products);
+
+// Event listener for pagination buttons
+const paginationContainer = document.querySelector(".pagination");
+paginationContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("pagination-btn")) {
+    currentPage = parseInt(event.target.dataset.page);
+    displayProducts(products);
+    renderPagination(products);
+  }
 });
 
-displayProduct = displayProduct.join("");
-productContainer.innerHTML = displayProduct;
+
+
