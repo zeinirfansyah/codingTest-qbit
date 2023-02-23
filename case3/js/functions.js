278ae77861a5f462053776ad59d0829679356475
zen <12.zeinirfansyah@gmail.com>
2023-02-23 15:43:09 +0700

    add: products

--- case3/js/functions.js
+++ case3/js/functions.js
@@ -1,56 +1,83 @@
-const fruits = [
+const products = [
   {
-    id: 1,
-    title: "Fruit 1",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 1,
+    fruitName: 'Apel',
+    fruitType: 'IMPORT',
+    stock: 10,
+    description: 'Apel adalah buah yang memiliki rasa manis dan segar. Apel juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
   {
-    id: 2,
-    title: "Fruit 2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 2,
+    fruitName: 'Kurma',
+    fruitType: 'IMPORT',
+    stock: 20,
+    description: 'Kurma adalah buah yang memiliki rasa manis dan segar. Kurma juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
   {
-    id: 3,
-    title: "Fruit 3",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 3,
+    fruitName: 'apel',
+    fruitType: 'IMPORT',
+    stock: 50,
+    description: 'Apel adalah buah yang memiliki rasa manis dan segar. Apel juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
   {
-    id: 4,
-    title: "Fruit 4",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 4,
+    fruitName: 'Manggis',
+    fruitType: 'LOCAL',
+    stock: 100,
+    description: 'Manggis adalah buah yang memiliki rasa manis dan segar. Manggis juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
   {
-    id: 5,
-    title: "Fruit 5",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 5,
+    fruitName: 'Jeruk Bali',
+    fruitType: 'LOCAL',
+    stock: 10,
+    description: 'Jeruk Bali adalah buah yang memiliki rasa manis dan segar. Jeruk Bali juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
   {
-    id: 6,
-    title: "Fruit 6",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
-    img: "assets/Developer activity-amico.svg",
+    fruitId: 5,
+    fruitName: 'KURMA',
+    fruitType: 'IMPORT',
+    stock: 20,
+    description: 'Kurma adalah buah yang memiliki rasa manis dan segar. Kurma juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
+  },
+  {
+    fruitId: 5,
+    fruitName: 'Salak',
+    fruitType: 'LOCAL',
+    stock: 150,
+    description: 'Salak adalah buah yang memiliki rasa manis dan segar. Salak juga memiliki kandungan vitamin yang baik untuk kesehatan tubuh.',
+    foto: '../assets/kurma.jpg'
   },
 ];
 
-const fruitContainer = document.querySelector(".fruits");
-let displayFruit = fruits.map(function (fruit) {
-  return `<div class="col-md-4">
-  <div class="card">
-    <img src="${fruit.img}" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">${fruit.title}</h5>
-      <p class="card-text">
-        ${fruit.desc}
-      </p>
-      <a href="#" class="btn btn-klik">Go somewhere</a>
-    </div>
+
+const itemsPerPage = 6;
+const numberOfPages = Math.ceil(products.length / itemsPerPage);
+const pagination = document.querySelector(".pagination");
+let currentPage = 1;
+
+const productContainer = document.querySelector(".products");
+let displayProduct = products.map(function (product) {
+  return `<div class="buah card mb-3 mx-auto">
+  <img src="${product.foto}" class="card-img-top"/>
+  <div class="card-body">
+    <h5 class="card-title">${product.fruitName}</h5>
+    <p class="card-text">
+      Stok: ${product.stock} <br>
+      <div class="fruit-description d-none d-lg-block">${product.description}</div>
+      <br>
+      <div class="tipeBuah">${product.fruitType}</div>
+    </p>
   </div>
 </div>`;
 });
-displayFruit = displayFruit.join("");
-projectContainer.innerHTML = displayFruit;
+
+displayProduct = displayProduct.join("");
+productContainer.innerHTML = displayProduct;
